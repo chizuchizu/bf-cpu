2338417857c9d7f20966f546c4ec556dd959dcd6
Yuma <ochiyuma.ochi@gmail.com>
2024-07-04 22:48:57 +0900

    update decode.py

--- decode.py
+++ decode.py
@@ -1,16 +1,17 @@
 # TODO: update
 inst_to_binray = {
-    ">": "00000000",
-    "<": "00000000",
-    "+": "00000000",
-    "-": "00000000",
-    ",": "00000000",
-    ".": "00000000",
-    "[": "00000000",
-    "]": "00000000",
+    ">": "00",
+    "<": "01",
+    "+": "02",
+    "-": "03",
+    "[": "04",
+    "]": "05",
+    ",": "06",
+    ".": "07",
+    " ": "08",
 }
 
 strings = input()
 
 for instruction in strings:
-    print(inst_to_binary[instruction])
+    print(inst_to_binray[instruction], end=" ")
