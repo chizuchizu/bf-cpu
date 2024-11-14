42676a8c47c68c01b536e02a11799df9dd11b994
doit6p <riku2888@gmail.com>
2024-11-14 16:09:11 +0900

    add Makefile and 74HC00 testbench, fix module name

---
+++ src/logicIC/74HC00_tb.sv
@@ -0,0 +1,41 @@
+module _74HC00_tb();
+
+reg [3:0] in1;
+reg [3:0] in2;
+wire [3:0] out;
+
+_74HC00 nand_instance(
+    .in1,
+    .in2, 
+    .out
+);
+
+initial begin
+    $dumpfile("74HC00.vcd");
+    $dumpvars(1, _74HC00_tb);
+
+    in1 = 4'b0000;
+    in2 = 4'b0000;
+    test_out(4'b1111);
+
+    #5 in1 = 4'b1111;
+    #5 test_out(4'b1111);
+
+    #5 in2 = 4'b0110;
+    #5 test_out(4'b1001);
+
+    #5 in1 = 4'b0100;
+    #5 test_out(4'b1011);
+
+    #5 $finish;
+end
+
+task test_out(
+    input [3:0] ans
+);
+begin
+    if (out != ans) $error("output must be %b but output is %b", ans, out);
+end
+endtask
+
+endmodule
