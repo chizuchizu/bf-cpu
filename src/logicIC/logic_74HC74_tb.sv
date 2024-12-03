1ae0a2d611e10b4d49536f8d4cf5804248ce697f
nekotsuna <rffkminato@yahoo.co.jp>
2024-12-03 17:14:20 +0900

    change 74HC74testbench: add test in pr = H and clr = H

--- src/logicIC/logic_74HC74_tb.sv
+++ src/logicIC/logic_74HC74_tb.sv
@@ -2,7 +2,7 @@
 
 module logic_74HC74_tb ();
 
-parameter PERIOD1 = 10;
+parameter PERIOD1 = 15;
 
 reg n_clr1, n_clr2, n_pr1, n_pr2, clk1, clk2, in1, in2;
 wire out1, n_out1, out2, n_out2;
@@ -17,9 +17,6 @@ end
 
 // D-FF1のテスト
 initial begin 
-  integer i;
-  i = 0;
-
   clk1 = 1'b0;
   n_clr1 = 1'b1;
   n_pr1 = 1'b1; 
@@ -29,27 +26,23 @@ initial begin
 
   #(PERIOD1) //pos
   // 立ち上がり時に値が0になることの確認
-    test_dff1(i, 1'b0, 1'b1);
-    i++;
+    test_dff1( 1'b0, 1'b1);
     in1 = 1'b1;
   #(PERIOD1) //neg
   // 立下り時に値が変わらないことの確認  
-    test_dff1(i, 1'b0, 1'b1);
-    i++;
+    test_dff1( 1'b0, 1'b1);
 
   in1 = 1'b1;
 
   #(PERIOD1) //pos
   // 立ち上がり時に値が1になることの確認
-    test_dff1(i, 1'b1, 1'b0);
-    i++;
+    test_dff1( 1'b1, 1'b0);
   #(PERIOD1) //neg
     
   n_clr1 = 1'b0;
   #1
   // CLRが有効になったことの確認
-    test_dff1(i, 1'b0, 1'b1);
-    i++;
+    test_dff1( 1'b0, 1'b1);
   #1
   n_clr1 = 1'b1;
 
@@ -61,11 +54,20 @@ initial begin
   n_pr1 = 1'b0;
   #1
   // PRが有効になったことの確認
-    test_dff1(i, 1'b1, 1'b0);
-    i++;
+    test_dff1( 1'b1, 1'b0);
   #1
   n_pr1 = 1'b1;
 
+  #(PERIOD1) //pos
+  #(PERIOD1) //neg
+
+  n_pr1 = 1'b0;
+  n_clr1 = 1'b0;
+
+  #1
+  // clrとprが有効になったときの動作確認
+    test_dff1( 1'b1, 1'b1);
+    
   #1
   $finish;
 end
@@ -76,15 +78,14 @@ initial begin
 end
 
 task test_dff1(
-  integer i,
   input e_out, 
   input e_n_out
 );
 begin
   if (e_out !== 1'bx && out1 !== e_out)
-    $error("test%d: out must be %d, but out is %d", i, e_out, out1);
+    $error("%t: out must be %d, but out is %d", $time, e_out, out1);
   if (e_n_out !== 1'bx && n_out1 !== e_n_out)
-    $error("test%d: ~out must be %d, but ~out is %d", i, e_n_out, n_out1);
+    $error("%t: ~out must be %d, but ~out is %d", $time , e_n_out, n_out1);
 end
 endtask
 
