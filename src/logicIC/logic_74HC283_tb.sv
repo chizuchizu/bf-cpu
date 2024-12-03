2aef3525b99c84bd7334c5224681f896458c73f8
doit6p <riku2888@gmail.com>
2024-12-03 17:07:00 +0900

    create: logic_74HC283_tb

---
+++ src/logicIC/logic_74HC283_tb.sv
@@ -0,0 +1,55 @@
+module logic_74HC283_tb();
+
+reg [3:0] a_in;
+reg [3:0] b_in;
+reg c_in;
+
+wire [3:0] sum_out;
+wire c_out;
+
+logic_74HC283 logic_74HC283_instance(
+    .a_in,
+    .b_in, 
+    .c_in,
+    .sum_out,
+    .c_out
+);
+
+initial begin
+    $dumpfile("logic_74HC283.vcd");
+    $dumpvars(1, logic_74HC283_tb);
+
+    a_in = 4'b0000;
+    b_in = 4'b0000;
+    c_in = 1'b0000;
+    #0  test_out(4'b0000, 1'b0);
+
+    #10 a_in = 4'b1111;
+        b_in = 4'b1111;
+        c_in = 1'b1;
+    #0  test_out(4'b1111, 1'b1);
+
+    #10 a_in = 4'b0000;
+        b_in = 4'b1111;
+        c_in = 1'b1;
+    #0  test_out(4'b0000, 1'b1);
+
+    #10 a_in = 4'b0111;
+        b_in = 4'b0001;
+        c_in = 1'b0;
+    #0  test_out(4'b1000, 1'b0);
+
+    #0 $finish;
+end
+
+task test_out(
+    input [3:0] sum_ans,
+    input c_ans
+);
+begin
+    if (sum_out != sum_ans) $error("sum_output must be %b but output is %b", ans, out);
+    if (c_out != c_ans) $error("c_output must be %b but output is %b", ans, out);
+end
+endtask
+
+endmodule
