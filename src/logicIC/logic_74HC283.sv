3adbaf33a4d8db2a435e9f4a7bdcf1aa5e60c147
doit6p <riku2888@gmail.com>
2024-12-03 17:35:41 +0900

    add logic_74HC283.sv

---
+++ src/logicIC/logic_74HC283.sv
@@ -0,0 +1,13 @@
+module logic_74HC283(
+    input [3:0] a_in,
+    input [3:0] b_in,
+    input c_in,
+    output [3:0] sum_out,
+    output c_out
+);
+    reg [4:0] sum;
+    
+    assign sum = {1'b0, a_in} + {1'b0, b_in} + {4'b0000, c_in};
+    assign sum_out = sum[3:0];
+    assign c_out = sum[4];
+endmodule
