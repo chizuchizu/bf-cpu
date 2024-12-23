0b3bf1185d8b6cb4db7f6529b5e9ddeda3e99251
nekotsuna <rffkminato@yahoo.co.jp>
2024-12-23 17:42:20 +0900

    fix: change initiallization process to change register directly

--- src/logicIC/logic_74HC74.sv
+++ src/logicIC/logic_74HC74.sv
@@ -15,20 +15,34 @@ module logic_74HC74(
 );
 
 reg reg1;
+reg n_reg1;
 reg reg2;
+reg n_reg2;
 
-assign out1 = reg1 & n_clr1 | ~n_pr1; 
-assign out2 = reg2 & n_clr2 | ~n_pr2;
+assign out1 = reg1;
+assign out2 = reg2;
 
-assign n_out1 = ~reg1 * n_pr1 + ~n_clr1; 
-assign n_out2 = ~reg2 * n_pr2 + ~n_clr2; 
+assign n_out1 = n_reg1;
+assign n_out2 = n_reg2;
 
-always @(posedge clk1) begin
-    reg1 <= in1;
+always @(posedge clk1 or negedge n_clr1 or negedge n_pr1) begin
+    if(~n_clr1 | ~n_pr1) begin
+      reg1 <= n_clr1 | ~n_pr1; 
+      n_reg1 <= ~n_clr1 | n_pr1;
+    end else begin
+      reg1 <= in1;
+      n_reg1 <= ~in1;
+    end
 end
 
-always @(posedge clk2) begin
-    reg2 <= in2;
+always @(posedge clk2 or negedge n_clr2 or negedge n_pr2) begin
+    if(~n_clr2 | ~n_pr2) begin
+      reg2 <= n_clr2 | ~n_pr2; 
+      n_reg2 <= ~n_clr2 | n_pr2;
+    end else begin
+      reg2 <= in2;
+      n_reg2 <= ~in2;
+    end
 end
 
 endmodule
