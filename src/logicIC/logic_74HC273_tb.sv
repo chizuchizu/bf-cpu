8ae59232fb6bb92eb9e8573492ae44f9f3b96943
doit6p <riku2888@gmail.com>
2024-12-23 17:27:31 +0900

    change 74HC273_tb: to check if the value is still 0 after clr

--- src/logicIC/logic_74HC273_tb.sv
+++ src/logicIC/logic_74HC273_tb.sv
@@ -21,43 +21,43 @@ initial begin
     clk = 1'b0;
     n_clr = 1'b1;
     in = 8'b00000000;
-
     #1
 
     #(PERIOD) // positive edge
         // 値がinと同じになっているか確認
         test_dff(8'b00000000);
-        in = 8'b11111111;
 
     #(PERIOD) // negative edge
         // 立ち下がり時に値が更新されていないか確認
-        test_dff(8'b00000000);
         in = 8'b11111111;
+        test_dff(8'b00000000);
 
     #(PERIOD) // positive edge
         // 立ち上がり時に値が同じになっているか確認
+        in = 8'b11111111;
         test_dff(8'b11111111);
-        in = 8'b00000000;
 
     #(PERIOD) // negative edge
         // 立ち下がり時に値が更新されていないか確認
+        in = 8'b00000000;
         test_dff(8'b11111111);
-        in = 8'b11111111;
-        n_clr = 1'b0;
 
     #(PERIOD) // positive edge
         // clrが有効か確認
-        test_dff(8'b00000000);
         in = 8'b11111111;
-        n_clr = 1'b1;
+        n_clr = 1'b0;
+        test_dff(8'b00000000);
 
     #(PERIOD) // negative edge
-        // clrを無効にした際、立ち下がり時値が更新されていないか確認
-        test_dff(8'b00000000);
+        // clrが1になった際、値が0のままか
         in = 8'b11111111;
+        n_clr = 1'b1;
+        test_dff(8'b00000000);
 
-    #(PERIOD) // positive edge
-        // 立ち上がり時に値が更新されるか確認
+    #(PERIOD) //positive edge
+        // 値が更新されるか確認
+        in = 8'b11111111;
+        n_clr = 1'b1;
         test_dff(8'b11111111);
 
     $finish;
@@ -69,7 +69,7 @@ initial begin
 end
 
 task test_dff(
-    input e_out
+    input [7:0] e_out
 );
 begin
     if (e_out !== 1'bx && out !== e_out)
