788752414382a32246a0204c8a9e416606b28c56
nekotsuna <rffkminato@yahoo.co.jp>
2025-01-02 16:37:25 +0900

    add general register

--- src/bfcpu/module/general_register.sv
+++ src/bfcpu/module/general_register.sv
@@ -1,3 +1,5 @@
+`include "../logicIC/logic_74HC161.sv"
+
 module general_register (
   input clk,
   input n_clr,
@@ -6,5 +8,37 @@ module general_register (
 
   output [7:0] out
 );
+
+wire co;
+
+wire [3:0] in1;
+wire [3:0] out1;
+logic_74HC161 logic_74HC161_1(
+  .clk,
+  .n_rst(n_clr),
+  .n_ld,
+  .enp(1'b0),
+  .ent(1'b0),
+  .in(in1),
+  .out(out1),
+  .co
+);
+
+wire [3:0] in2;
+wire [3:0] out2;
+logic_74HC161 logic_74HC161_2(
+  .clk,
+  .n_rst(n_clr),
+  .n_ld,
+  .enp(1'b0),
+  .ent(1'b0),
+  .in(in2),
+  .out(out2),
+  .co
+);
+
+assign in1 = in[3:0];
+assign in2 = in[7:4];
+assign out = {out2, out1};
 
 endmodule
