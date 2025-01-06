42822c0860d05ed2c075d0a1f221f3892e92ffe6
doit6p <riku2888@gmail.com>
2025-01-06 17:30:14 +0900

    fix: Naming convention

--- src/bfcpu/module/alu.sv
+++ src/bfcpu/module/alu.sv
@@ -14,83 +14,83 @@ module alu(
   output [7:0] out
 );
 
-wire [3:0] and_in1;
-wire [3:0] and_in2;
-wire [3:0] and_out;
+wire [3:0] and_in1_1;
+wire [3:0] and_in2_1;
+wire [3:0] and_out_1;
 logic_74HC08 logic_74HC08_1(
-    .in1(and_in1),
-    .in2(and_in2),
-    .out(and_out)
+    .in1(and_in1_1),
+    .in2(and_in2_1),
+    .out(and_out_1)
 );
 
-wire [5:0] not_in;
-wire [5:0] not_out;
+wire [5:0] not_in_1;
+wire [5:0] not_out_1;
 logic_74HC04 logic_74HC04_1(
-    .in(not_in),
-    .out(not_out)
+    .in(not_in_1),
+    .out(not_out_1)
 );
 
-wire tri_n_g1;
-wire tri_n_g2;
-wire [7:0] tri_in;
-wire[7:0] tri_out;
+wire tri_n_g1_1;
+wire tri_n_g2_1;
+wire [7:0] tri_in_1;
+wire[7:0] tri_out_1;
 logic_74HC541 logic_74HC541_1(
-    .n_g1(tri_n_g1),
-    .n_g2(tri_n_g2),
-    .in(tri_in),
-    .out(tri_out)
+    .n_g1(tri_n_g1_1),
+    .n_g2(tri_n_g2_1),
+    .in(tri_in_1),
+    .out(tri_out_1)
 );
 
-wire [3:0] fa1_a_in;
-wire [3:0] fa1_b_in;
-wire fa1_c_in;
-wire [3:0] fa1_sum_out;
-wire fa1_c_out;
+wire [3:0] sum_a_in_1;
+wire [3:0] sum_b_in_1;
+wire sum_c_in_1;
+wire [3:0] sum_sum_out_1;
+wire sum_c_out_1;
 logic_74HC283 logic_74HC283_1(
-    .a_in(fa1_a_in),
-    .b_in(fa1_b_in),
-    .c_in(fa1_c_in),
-    .sum_out(fa1_sum_out),
-    .c_out(fa1_c_out)
+    .a_in(sum_a_in_1),
+    .b_in(sum_b_in_1),
+    .c_in(sum_c_in_1),
+    .sum_out(sum_sum_out_1),
+    .c_out(sum_c_out_1)
 );
 
-wire [3:0] fa2_a_in;
-wire [3:0] fa2_b_in;
-wire fa2_c_in;
-wire [3:0] fa2_sum_out;
-wire fa2_c_out;
+wire [3:0] sum_a_in_2;
+wire [3:0] sum_b_in_2;
+wire sum_c_in_2;
+wire [3:0] sum_sum_out_2;
+wire sum_c_out_2;
 logic_74HC283 logic_74HC283_2(
-    .a_in(fa2_a_in),
-    .b_in(fa2_b_in),
-    .c_in(fa2_c_in),
-    .sum_out(fa2_sum_out),
-    .c_out(fa2_c_out)
+    .a_in(sum_a_in_2),
+    .b_in(sum_b_in_2),
+    .c_in(sum_c_in_2),
+    .sum_out(sum_sum_out_2),
+    .c_out(sum_c_out_2)
 );
 
 
-assign not_in[0] = decrement; // not_out[0] == ~decrement
-assign not_in[1] = increment; // not_out[1] == ~increment
+assign not_in_1[0] = decrement; // not_out[0] == ~decrement
+assign not_in_1[1] = increment; // not_out[1] == ~increment
 
-assign and_in1[0] = decrement;
-assign and_in2[0] = increment; // and_out[0] == decrement & increment
+assign and_in1_1[0] = decrement;
+assign and_in2_1[0] = increment; // and_out[0] == decrement & increment
 
-assign and_in1[1] = and_out[0];
-assign and_in2[1] = nochange; // and_out[1] == decrement & increment & nochange
+assign and_in1_1[1] = and_out_1[0];
+assign and_in2_1[1] = nochange; // and_out[1] == decrement & increment & nochange
 
-assign fa1_a_in = a[3:0];
-assign fa2_a_in = a[7:4];
+assign sum_a_in_1 = a[3:0];
+assign sum_a_in_2 = a[7:4];
 
-assign fa1_b_in = {4{not_out[0]}};
-assign fa2_b_in = {4{not_out[0]}};
+assign sum_b_in_1 = {4{not_out_1[0]}};
+assign sum_b_in_2 = {4{not_out_1[0]}};
 
-assign fa1_c_in = not_out[1];
-assign fa2_c_in = fa1_c_out; // carry
+assign sum_c_in_1 = not_out_1[1];
+assign sum_c_in_2 = sum_c_out_1; // carry
 
-assign tri_n_g1 = and_out[1];
-assign tri_n_g2 = and_out[1];
-assign tri_in = {fa2_sum_out, fa1_sum_out};
+assign tri_n_g1_1 = and_out_1[1];
+assign tri_n_g2_1 = and_out_1[1];
+assign tri_in_1 = {sum_sum_out_2, sum_sum_out_1};
 
-assign out = tri_out;
+assign out = tri_out_1;
 endmodule
 
 `endif
--- src/bfcpu/module/zero_detector.sv
+++ src/bfcpu/module/zero_detector.sv
@@ -8,46 +8,46 @@ module zero_detector(
 	output out
 );
 
-wire [3:0] in1_1;
-wire [3:0] in2_1;
-wire [3:0] out_1;
+wire [3:0] or_in1_1;
+wire [3:0] or_in2_1;
+wire [3:0] or_out_1;
 logic_74HC32 logic_74HC32_1(
-    .in1(in1_1),
-    .in2(in2_1),
-    .out(out_1)
+    .in1(or_in1_1),
+    .in2(or_in2_1),
+    .out(or_out_1)
 );
 
-wire [3:0] in1_2;
-wire [3:0] in2_2;
-wire [3:0] out_2;
+wire [3:0] or_in1_2;
+wire [3:0] or_in2_2;
+wire [3:0] or_out_2;
 logic_74HC32 logic_74HC32_2(
-    .in1(in1_2),
-    .in2(in2_2),
-    .out(out_2)
+    .in1(or_in1_2),
+    .in2(or_in2_2),
+    .out(or_out_2)
 );
 
-assign in1_1[0] = in[0];
-assign in2_1[0] = in[1]; // out_1[0] = in[0] | in[1]
+assign or_in1_1[0] = in[0];
+assign or_in2_1[0] = in[1]; // or_out_1[0] = in[0] | in[1]
 
-assign in1_1[1] = in[2];
-assign in2_1[1] = in[3]; // out_1[1] = in[2] | in[3]
+assign or_in1_1[1] = in[2];
+assign or_in2_1[1] = in[3]; // or_out_1[1] = in[2] | in[3]
 
-assign in1_1[2] = in[4];
-assign in2_1[2] = in[5]; // out_1[2] = in[4] | in[5]
+assign or_in1_1[2] = in[4];
+assign or_in2_1[2] = in[5]; // or_out_1[2] = in[4] | in[5]
 
-assign in1_1[3] = in[6];
-assign in2_1[3] = in[7]; // out_1[3] = in[6] | in[7]
+assign or_in1_1[3] = in[6];
+assign or_in2_1[3] = in[7]; // or_out_1[3] = in[6] | in[7]
 
-assign in1_2[0] = out_1[0];
-assign in2_2[0] = out_1[1];
+assign or_in1_2[0] = or_out_1[0];
+assign or_in2_2[0] = or_out_1[1];
 
-assign in1_2[1] = out_1[2];
-assign in2_2[1] = out_1[3];
+assign or_in1_2[1] = or_out_1[2];
+assign or_in2_2[1] = or_out_1[3];
 
-assign in1_2[2] = out_2[0];
-assign in2_2[2] = out_2[1];
+assign or_in1_2[2] = or_out_2[0];
+assign or_in2_2[2] = or_out_2[1];
 
-assign out = out_2[2];
+assign out = or_out_2[2];
 
 endmodule
 
