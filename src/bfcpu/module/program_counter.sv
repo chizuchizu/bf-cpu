6863d1eb63becbe99bcbd08075a6c1dbbc8b4817
nekotsuna <rffkminato@yahoo.co.jp>
2025-03-21 03:37:05 +0900

    add program counter testbench

---
+++ src/bfcpu/module/program_counter.sv
@@ -0,0 +1,19 @@
+`ifndef PROGRAM_COUNTER
+`define PROGRAM_COUNTER
+
+module program_counter (
+  input clk,
+  input clk_switch,
+  input clk_manual,
+  input n_clr,
+  input n_pc_ld,
+  input [7:0] in,
+
+  output [7:0] out
+);
+
+
+
+endmodule
+
+`endif
---
+++ src/bfcpu/module/program_counter_tb.sv
@@ -0,0 +1,111 @@
+`timescale 1ns / 1ps
+
+module program_counter_tb ();
+
+parameter PERIOD = 10;
+
+reg clk, clk_switch, clk_manual;
+reg n_clr, n_pc_ld;
+reg [7:0] in;
+wire [7:0] out;
+
+program_counter program_counter_tb(
+  .*
+);
+
+initial begin
+  $dumpfile("program_counter.vcd");
+  $dumpvars(0, program_counter_tb);
+end
+
+always #(PERIOD) begin
+  clk <= ~clk;
+end
+
+initial begin
+  clk <= 1'b0;
+  clk_switch <= 1'b0;
+  clk_manual <= 1'bz;
+  n_clr <= 1'b1;
+  n_pc_ld <= 1'b1;
+  in <= 8'b00000000;
+
+  #1
+  n_clr <= 1'b0;
+
+  #1
+  n_clr <= 1'b1;
+
+  // n_clr が作動するかの確認
+  test_dff(8'b00000000);
+
+  // clk のマニュアル動作の確認
+  #1
+  clk_switch <= 1'b1;
+
+  #PERIOD
+  clk_manual <= 1'b1;
+
+  #PERIOD
+  clk_manual <= 1'b0;
+
+  test_dff(8'b00000001);
+
+  #PERIOD
+  clk_manual <= 1'b1;
+
+  #PERIOD
+  clk_manual <= 1'b0;
+  
+  test_dff(8'b00000010);
+
+  #1
+  clk_manual <= 1'bz;
+
+  #1
+  clk_switch <= 1'b0;
+
+  #PERIOD
+  #PERIOD
+  // 通常のclkでカウントアップの確認
+  test_dff(8'b00000011); 
+
+  n_pc_ld <= 1'b0;
+  in <= 8'b00001111;
+
+  #PERIOD
+  #PERIOD
+  // n_pc_ldをアサートしたとき、値が入力されるかの確認
+  test_dff(8'b00001111); 
+
+  n_pc_ld <= 1'b1;
+   
+  #PERIOD
+  #PERIOD
+  // 5bit目以降に値が代入されるかの確認
+  test_dff(8'b00010000);
+  
+  n_pc_ld <= 1'b0;
+  in <= 8'b11111111;
+
+  #PERIOD
+  #PERIOD
+  n_pc_ld <= 1'b1;
+
+  #PERIOD
+  #PERIOD
+  // 値が繰り上がって0になることの確認
+  test_dff(8'b00000000);
+
+  $finish;
+end
+
+task test_dff(
+  input [7:0] e_out
+);
+  if(e_out !== 8'bxxxxxxxx && out !== e_out) begin
+    $error("%t: out must be %b, but out is %b",$time, e_out, out);
+  end 
+endtask
+
+endmodule
