7b21dc8c5350318e268f4ff42e1d328b2c202c2b
nekotsuna <rffkminato@yahoo.co.jp>
2025-03-22 16:33:49 +0900

    add divider

--- src/bfcpu/module/divider.sv
+++ src/bfcpu/module/divider.sv
@@ -1,6 +1,8 @@
 `ifndef DIVIDER
 `define DIVIDER
 
+`include "../logicIC/logic_74HC74.sv"
+
 module divider (
   input clk,
   input n_clr,
@@ -8,6 +10,45 @@ module divider (
   output half_clk,
   output quarter_clk
 );
+
+wire logic_74HC74_n_clr1_1;
+wire logic_74HC74_n_pr1_1;
+wire logic_74HC74_clk1_1;
+wire logic_74HC74_in1_1;
+wire logic_74HC74_out1_1;
+wire logic_74HC74_n_out1_1;
+wire logic_74HC74_n_clr2_1;
+wire logic_74HC74_n_pr2_1;
+wire logic_74HC74_clk2_1;
+wire logic_74HC74_in2_1;
+wire logic_74HC74_out2_1;
+wire logic_74HC74_n_out2_1;
+logic_74HC74 logic_74HC74_1(
+  .n_clr1(logic_74HC74_n_clr1_1),
+  .n_pr1(logic_74HC74_n_pr1_1),
+  .clk1(logic_74HC74_clk1_1),
+  .in1(logic_74HC74_in1_1),
+  .out1(logic_74HC74_out1_1),
+  .n_out1(logic_74HC74_n_out1_1),
+  .n_clr2(logic_74HC74_n_clr2_1),
+  .n_pr2(logic_74HC74_n_pr2_1),
+  .clk2(logic_74HC74_clk2_1),
+  .in2(logic_74HC74_in2_1),
+  .out2(logic_74HC74_out2_1),
+  .n_out2(logic_74HC74_n_out2_1)
+);
+
+assign half_clk = logic_74HC74_out1_1;
+assign quarter_clk = logic_74HC74_out2_1;
+
+assign logic_74HC74_n_clr1_1 = n_clr;
+assign logic_74HC74_n_pr1_1 = 1'b1;
+assign logic_74HC74_clk1_1 = clk;
+assign logic_74HC74_in1_1 = logic_74HC74_n_out1_1;
+assign logic_74HC74_n_clr2_1 = n_clr;
+assign logic_74HC74_n_pr2_1 = 1'b1;
+assign logic_74HC74_clk2_1 = logic_74HC74_out1_1;
+assign logic_74HC74_in2_1 = logic_74HC74_n_out2_1;
 
 endmodule
 
